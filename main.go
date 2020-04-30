fb8cee2b267e9f26f38042db15ab062b324c9107
alifudin-a <alifudinalfarizi@gmail.com>
2020-04-30 12:56:21 +0700

    added authorization auth0

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 
 	"github.com/alifudin-a/todo-app-go-angular/handlers"
+	"github.com/auth0-community/go-auth0"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/square/go-jose.v2"
+)
+
+var (
+	audience string
+	domain   string
 )
 
 func main() {
+
+	setAuth0Variable()
 	r := gin.Default()
+
+	// This will ensure that the angular files are served correctly
 	r.NoRoute(func(c *gin.Context) {
 		dir, file := path.Split(c.Request.RequestURI)
 		ext := filepath.Ext(file)
@@ -20,13 +34,43 @@ func main() {
 		}
 	})
 
-	r.GET("/todo", handlers.GetTodoListHandler)
-	r.POST("/todo", handlers.AddTodoHandler)
-	r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
-	r.PUT("/todo", handlers.CompleteTodoHandler)
+	authorized := r.Group("/")
+	authorized.Use(authRequired())
+	authorized.GET("/todo", handlers.GetTodoListHandler)
+	authorized.POST("/todo", handlers.AddTodoHandler)
+	authorized.DELETE("/todo/:id", handlers.DeleteTodoHandler)
+	authorized.PUT("/todo", handlers.CompleteTodoHandler)
 
-	err := r.Run(":8080")
+	err := r.Run(":3000")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func setAuth0Variable() {
+	audience = os.Getenv("AUTH0_API_IDENTIFIER")
+	domain = os.Getenv("AUTH0_DOMAIN")
+}
+
+// ValidateRequest will verify that a token received from an http request is valid ang signed by Auth0
+func authRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var auth0Domain = "https://" + domain + "/"
+		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
+		configuration := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
+		validator := auth0.NewValidator(configuration, nil)
+
+		_, err := validator.ValidateRequest(c.Request)
+		if err != nil {
+			log.Println(err)
+			terminateWithError(http.StatusUnauthorized, "token is not valid", c)
+			return
+		}
+		c.Next()
+	}
+}
+
+func terminateWithError(statusCode int, message string, c *gin.Context) {
+	c.JSON(statusCode, gin.H{"error": message})
+	c.Abort()
 }
