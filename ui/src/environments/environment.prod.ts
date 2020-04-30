46bbfb2966b1fc7e64df2d7ff470e04095894240
alifudin-a <alifudinalfarizi@gmail.com>
2020-04-30 15:14:00 +0700

    angular todo app

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: 'dev-ydr2hi3p.auth0.com',
+  clientId: 'scaZNqkMtjqaPkA1CAPSbvXQxhjF5nLk',
+  audience: 'https://my-golang-api'
+};
---
+++ ui/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  gateway: 'http://localhost:3000',
+  callback: 'http://localhost:4200/callback',
+  domain: 'dev-ydr2hi3p.auth0.com',
+  clientId: 'scaZNqkMtjqaPkA1CAPSbvXQxhjF5nLk',
+  audience: 'https://my-golang-api'
+};
